722ee1a3b076f48c09ff081678ad10f9f916c1e3
jacob hoefer <jacob.hoefer@gmail.com>
2023-12-01 14:44:32 -0500

    reworked site to be one page

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,18 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
-import Graphics from "./pages/Graphic_design";
-import Video from "./pages/Video";
-import Projects from "./pages/Projects";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <main className="container-fluid"> */}
+     
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/graphics" element={<Graphics />} />
-          <Route path="/video" element={<Video />} />
-          <Route path="/projects" element={<Projects />} />
         </Routes>
-      {/* </main> */}
+
 
       <Footer />
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 import RightFace from "../assets/right_face.png";
 import LeftFace from "../assets/left_face_full.png";
 import LeftLines from "../assets/left-lines.png";
-import { NavLink } from "react-router-dom";
+
+import oToole from "../assets/FINTAN O'TOOLE instagram.png";
+import unruly1 from "../assets/unruly1.png";
+import autumn from "../assets/autumn_work.png";
+import downstairs from "../assets/downstairs sign redux CAPS.png";
+import flat from "../assets/flat,373x,075,f.jpg";
+import MH from "../assets/MH.png";
 
 function Home() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -16,15 +30,38 @@ function Home() {
       <section className="cta">
         <h1>Jacob Hoefer</h1>
         <p>from png to pwa and everthying inbetween</p>
+        <section>
+          <Swiper
+            className="graphics"
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={50}
+            slidesPerView={3}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+          >
+            <SwiperSlide>
+              <img src={oToole} alt="Fintan Otoole" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={autumn} alt="Labyrinth Books" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={flat} alt="Hell" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={MH} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={unruly1} alt="" />
+            </SwiperSlide>
+          </Swiper>
+        </section>
         <div className="dropdown">
           <h2 onClick={toggleDropdown}>Check Out My Work</h2>
-          {isDropdownOpen && (
-            <div className="dropdown-content">
-              <NavLink to="/projects">Full Stack Developmment </NavLink>
-              <NavLink to="/graphics">Graphic Design</NavLink>
-              <NavLink to="/video">Video/Audio Editing</NavLink>
-            </div>
-          )}
+          {isDropdownOpen && <div className="dropdown-content"></div>}
         </div>
       </section>
       <div className="face">
